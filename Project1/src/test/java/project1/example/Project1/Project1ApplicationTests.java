034f1e224ecd5030f6e54154a385857fa29939fd
Leticia Affonso <le.affonsosoares.gmail.com>
2023-09-14 19:23:58 -0300

    Criação Projeto Spring

---
+++ Project1/src/test/java/project1/example/Project1/Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package project1.example.Project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
