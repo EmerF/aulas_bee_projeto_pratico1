b1d77ad4de0b56a6316a10f37cc551d4e69ac372
chrispsilva99 <christian.silva@ab-inbev.com>
2023-09-14 18:04:13 -0300

    new: Criação do projeto

---
+++ src/main/java/com/ambev/ProjetoEmpower/ProjetoEmpowerApplication.java
@@ -0,0 +1,13 @@
+package com.ambev.ProjetoEmpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEmpowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEmpowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambev/ProjetoEmpower/ProjetoEmpowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambev.ProjetoEmpower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEmpowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
