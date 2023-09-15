06abcf93a471be82b54f9f51537e0ad16c96de75
GraciiFerreira <graciele.af26@gmail.com>
2023-09-15 11:56:09 -0300

    Feat: criacao de teste com selenium

---
+++ projeto/src/test/java/com/projeto/projeto/selenium/AmbevWebsiteTest.java
@@ -0,0 +1,35 @@
+package com.projeto.projeto.selenium;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AmbevWebsiteTest {
+
+    @Test
+    public void testLogin() throws MalformedURLException {
+        // Set the URL of the Selenium Server
+
+        String seleniumHubURL = "http://localhost:4444/wd/hub";
+
+        // Define desired capabilities for the browser and platform
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("firefox"); // Use "firefox" for Firefox browser
+        capabilities.setCapability("version", "76.0");
+        capabilities.setPlatform(Platform.ANY);
+
+        // Create a Remote WebDriver instance
+        WebDriver driver = new RemoteWebDriver(new URL(seleniumHubURL), capabilities);
+        driver.get("https://www.ambev.com.br/");
+        // Your test code here...
+        // senha do sessions do selenium (http://localhost:4444): secret
+        // Close the WebDriver
+        driver.quit();
+
+    }
+}
