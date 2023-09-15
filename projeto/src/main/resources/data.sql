dc393d8cb989e69669341ca629c1d6abfd4406db
regislaine <regislaine.regis@ab-inbev.com>
2023-09-15 13:37:56 -0300

    fix: finalizando o relacionamento das tabelas e o innerjoi

--- projeto/src/main/resources/data.sql
+++ projeto/src/main/resources/data.sql
@@ -1,6 +1,7 @@
--- Create a sample table product
-CREATE TABLE IF NOT EXISTS product (
-id INT AUTO_INCREMENT PRIMARY KEY,
+DROP TABLE IF EXISTS product;
+
+CREATE TABLE product (
+product_id INT AUTO_INCREMENT PRIMARY KEY,
 name_product VARCHAR(50),
 price_product DECIMAL(10,2) NOT NULL DEFAULT 0,
 description_product VARCHAR(100)
@@ -10,14 +11,14 @@ description_product VARCHAR(100)
 
 INSERT INTO product (name_product, price_product, description_product)
 VALUES
-('caderno',10.80, 'caderno ultra moderno de desenho'),
-('lapis de cor',30.80, 'lapis de cor aquarelavel 36 cores'),
-('caneta borracha', 16.80 , 'caneta borracha com 3 refil');
+('skol',10.80, 'caixa com 12 latas'),
+('corona',36.80, 'pack com 6 garrafas'),
+('budweiser', 26.80 , 'pack com 6 garrafas');
 
 -- update name 2 products
-UPDATE product SET name_product = 'caneta glitter' WHERE id = 1;
+UPDATE product SET name_product = 'brahma' WHERE product_id = 1;
 
-UPDATE product SET name_product = 'lapiseira' WHERE id = 2;
+UPDATE product SET name_product = 'brahma duplo malte' WHERE product_id = 2; 
 
 -- insert column with name origem
 ALTER TABLE product ADD COLUMN origem VARCHAR(100);
@@ -26,12 +27,40 @@ ALTER TABLE product ADD COLUMN origem VARCHAR(100);
 
 INSERT INTO product (name_product, price_product, description_product, origem)
 VALUES
-('lapiseira',18.90, 'lapiseira 0.7', 'made in china');
+('stella',28.90, 'pack com 6 garrafas', 'made in brazil');
 
 
 -- Create a sample table tipo product
-CREATE TABLE IF NOT EXISTS type_product (
-id INT AUTO_INCREMENT PRIMARY KEY,
+DROP TABLE IF EXISTS type_product;
+CREATE TABLE type_product (
+type_id INT PRIMARY KEY AUTO_INCREMENT,
 name_type VARCHAR(50),
-categorie VARCHAR(100)
+category VARCHAR(100)
 );
+
+-- Create a relationship
+ALTER TABLE product ADD COLUMN type_product_id INT;
+ALTER TABLE product ADD CONSTRAINT fk_type_product FOREIGN KEY (type_product_id)
+REFERENCES type_product(type_id);
+
+-- Insert 3 type of product
+INSERT INTO type_product(name_type, category) 
+VALUES
+('Vienna Lager', 'Viena'),
+('Pilsen', 'República Tcheca '),
+('Bock', 'Alemanha');
+
+UPDATE product  SET type_product_id = 1 
+WHERE product_id = 1;
+UPDATE product SET type_product_id = 2
+WHERE product_id = 2;
+
+-- SELECT product and type_product
+SELECT * FROM product;
+
+SELECT * FROM type_product;
+
+-- SELECT USE DE INNER JOIN 
+
+SELECT DISTINCT product.name_product, product.price_product AS tipo FROM product 
+INNER JOIN type_product ON product.type_product_id = type_product_id;
