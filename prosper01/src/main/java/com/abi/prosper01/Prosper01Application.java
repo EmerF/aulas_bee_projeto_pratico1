50a088f00c7ea9517e0f321b44e8d075154fb1f0
lukas-95 <lucrecmen@gmail.com>
2023-09-15 20:10:02 -0300

    create spring project and add .gitignore

---
+++ prosper01/src/main/java/com/abi/prosper01/Prosper01Application.java
@@ -0,0 +1,13 @@
+package com.abi.prosper01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prosper01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prosper01Application.class, args);
+	}
+
+}
---
+++ prosper01/src/test/java/com/abi/prosper01/Prosper01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abi.prosper01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prosper01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
