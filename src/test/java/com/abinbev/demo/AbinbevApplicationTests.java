80f5746ed00c36f3158eea92ab5c7b9f9cab7897
Samuel Marques <samuel@GHBRLPE0AK3CR.home>
2023-09-15 09:08:28 -0300

    Arquivo mongo

--- src/test/java/com/abinbev/demo/AbinbevApplicationTests.java
+++ src/test/java/com/abinbev/demo/AbinbevApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AbinbevApplicationTests {
 
 	@Test
 	void contextLoads() {
