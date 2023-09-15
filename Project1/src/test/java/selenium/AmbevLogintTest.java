4287abc27d3ba587473327a9d5daf7699cee9e90
Leticia Affonso <le.affonsosoares.gmail.com>
2023-09-15 19:28:24 -0300

    Selenium

---
+++ Project1/src/test/java/selenium/AmbevLogintTest.java
@@ -0,0 +1,35 @@
+package selenium;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AmbevLogintTest {
+
+        @Test
+        public void testLogin() throws MalformedURLException {
+            // Set the URL of the Selenium Server
+
+            String seleniumHubURL = "http://localhost:4444/wd/hub";
+
+            // Define desired capabilities for the browser and platform
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName("firefox"); // Use "firefox" for Firefox browser
+            capabilities.setCapability("version", "76.0");
+            capabilities.setPlatform(Platform.ANY);
+
+            // Create a Remote WebDriver instance
+            WebDriver driver = new RemoteWebDriver(new URL(seleniumHubURL), capabilities);
+            driver.get("https://www.ambev.com.br");
+            // Your test code here...
+            // Close the WebDriver
+            //driver.quit();
+
+        }
+}
+
