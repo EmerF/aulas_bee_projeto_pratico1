03941de77151f3cf08587cf1fca468e7ca3493d6
Mike <marquezini.mike@outlook.com>
2023-09-15 21:20:25 -0300

    Desafio One PROSPER

---
+++ AB-InBev/src/main/java/com/prosper/ABInBev/AbInBevApplication.java
@@ -0,0 +1,13 @@
+package com.prosper.ABInBev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbInBevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbInBevApplication.class, args);
+	}
+
+}
