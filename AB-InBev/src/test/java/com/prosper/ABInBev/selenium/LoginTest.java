856d03de2290eb276d33ecd2d46dfe07ee289e49
Mike <marquezini.mike@outlook.com>
2023-09-15 21:18:11 -0300

    Test Selenium

---
+++ AB-InBev/src/test/java/com/prosper/ABInBev/selenium/LoginTest.java
@@ -0,0 +1,34 @@
+package com.prosper.ABInBev.selenium;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class LoginTest {
+    @Test
+    public void testLogin() throws MalformedURLException {
+        // Set the URL of the Selenium Server
+
+        String seleniumHubURL = "http://localhost:4444/wd/hub";
+
+        // Define desired capabilities for the browser and platform
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("firefox"); // Use "firefox" for Firefox browser
+        capabilities.setCapability("version", "76.0");
+        capabilities.setPlatform(Platform.ANY);
+
+        // Create a Remote WebDriver instance
+        WebDriver driver = new RemoteWebDriver(new URL(seleniumHubURL), capabilities);
+        driver.get("http://globo.com");
+        // Your test code here...
+        // senha do sessions do selenium (http://localhost:4444): secret
+        // Close the WebDriver
+        //driver.quit();
+
+    }
+}
