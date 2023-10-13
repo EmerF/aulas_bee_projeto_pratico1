068b92b93518319ffce83c811081e5263bc4a773
Gustavo Silva <contato@gustavodcs.com>
2023-10-13 12:44:55 -0300

    feat: add selenium test

---
+++ src/test/java/br/com/gustavo/trabalho1/selenium/OpenAmbevTest.java
@@ -0,0 +1,26 @@
+package br.com.gustavo.trabalho1.selenium;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+public class OpenAmbevTest {
+    @Test
+    public void testLogin() throws MalformedURLException {
+
+        String seleniumHubURL = "http://localhost:4444/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("firefox");
+        capabilities.setCapability("version", "76.0");
+        capabilities.setPlatform(Platform.ANY);
+
+        WebDriver driver = new RemoteWebDriver(new URL(seleniumHubURL), capabilities);
+        driver.get("https://www.ambev.com.br/");
+        driver.quit();
+    }
+}
