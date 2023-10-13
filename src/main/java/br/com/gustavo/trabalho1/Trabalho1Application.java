ab21946a09fc6e607a7a34bdb9630efe19f32cc8
Gustavo Silva <contato@gustavodcs.com>
2023-10-13 12:31:04 -0300

    feat: initiate project

---
+++ src/main/java/br/com/gustavo/trabalho1/Trabalho1Application.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.trabalho1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalho1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavo/trabalho1/Trabalho1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.trabalho1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
